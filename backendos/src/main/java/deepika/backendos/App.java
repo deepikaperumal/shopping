5e52dbf480d9f1d232ad00850883689aebc7bea3
Deepika <deepikaperumal95@gmail.com>
2021-03-15 18:36:55 +0530

    My Project Setup Completed Successfully

---
+++ backendos/src/main/java/deepika/backendos/App.java
@@ -0,0 +1,13 @@
+package deepika.backendos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
